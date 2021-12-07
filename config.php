636b9749388fd692d7b4cc6cd5477335e33f9a37
SilentEarth32 <stephenmg3250@gmail.com>
2021-12-06 19:30:06 -0600

    php

---
+++ config.php
@@ -0,0 +1,10 @@
+
+    <?php  
+    /* Database Connection */  
+    $sDbHost = 'localhost' ;  
+    $sDbName = 'mcn' ;  
+    $sDbUser = 'root';  
+    $sDbPud = '';  
+    $ Conn = mysql_connect ($sDbHost, $sDbUser, $sDbPwd);  
+    mysql_select_db ($sDbName, $Conn);  
+    ?>  
