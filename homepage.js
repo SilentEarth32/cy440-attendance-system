203286094b9769186bda120ecddb8b7a2181cc6a
SilentEarth32 <stephenmg3250@gmail.com>
2021-12-06 19:26:39 -0600

    first

---
+++ homepage.js
@@ -0,0 +1,7 @@
+function addStudent() {
+
+}
+
+function viewPast() {
+  
+}
---
+++ login.js
@@ -0,0 +1,17 @@
+function signIn(){
+  
+    // Email accessed via email.value
+    // Password accessed via password.value
+  
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+  
+    // For testing:
+    /*
+    console.log(email.value);
+    console.log(password.value);
+    */
+    
+    window.open("homepage.html", "_self");
+  
+}
