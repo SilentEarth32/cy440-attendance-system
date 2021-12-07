bbd553238b4ed949f855e8a4ce78714ef0639772
SilentEarth32 <stephenmg3250@gmail.com>
2021-12-06 22:33:55 -0600

    database functioning

--- config.php
+++ config.php
@@ -1,8 +1,45 @@
-<?php    
-    
-include "connection.php";    
-    
-$sql = "select * from student";    
-$result = mysql_query($sql);    
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "cy440_database";
 
-?> 
+$tbl_name="request_quote"; //table name
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+/*
+$sql = "INSERT INTO student (name)
+VALUES ('Tyler')";
+
+if ($conn->query($sql) === TRUE) {
+  echo " New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+*/
+$checkbox1=$_POST['attendanceRecord'];
+$chk="";
+foreach($checkbox1 as $chk1)
+   {
+      $chk .= $chk1.",";
+   }
+$in_ch=mysqli_query($conn,"insert into student (name) values ('$chk')");
+if($in_ch==1)
+   {
+      echo'<script>alert("Inserted Successfully")</script>';
+   }
+else
+   {
+      echo'<script>alert("Failed To Insert")</script>';
+   }
+
+
+$conn->close();
+?>
--- login.js
+++ login.js
@@ -1,24 +1,24 @@
 function signIn(){
   
-    // Email accessed via email.value
-    // Password accessed via password.value
+  // Email accessed via email.value
+  // Password accessed via password.value
+
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+
+  var usernameActual = ""; // add username
+  var passwordActual = ""; // add password
+
+  // For testing:
+  /*
+  console.log(username.value);
+  console.log(password.value);
+  */
   
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
-  
-    var usernameActual = ""; // add username
-    var passwordActual = ""; // add password
-  
-    // For testing:
-    /*
-    console.log(username.value);
-    console.log(password.value);
-    */
-    
-    if (username.value == usernameActual && password.value == passwordActual) {
-      window.open("homepage.html", "_self");
-    } else {
-      window.alert("Access denied. Please try again.");
-    }
-  
+  if (username.value == usernameActual && password.value == passwordActual) {
+    window.open("homepage.html", "_self");
+  } else {
+    window.alert("Access denied. Please try again.");
   }
+
+}
